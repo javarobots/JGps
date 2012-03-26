e7b1ab5e74c12d14931cff19295b1120f659c7de
javarobots74 <javarobots74@gmail.com>
2012-03-26 16:53:31 -0600

    New object to represent UTC time

---
+++ src/gps/data/UTCTime.java
@@ -0,0 +1,37 @@
+package gps.data;
+
+/**
+ *
+ * @author javarobots
+ */
+public class UTCTime {
+    
+    private int mHour;
+    private int mMinute;
+    private int mSecond;
+
+    public int getHour() {
+        return mHour;
+    }
+
+    public void setHour(int hour) {
+        mHour = hour;
+    }
+
+    public int getMinute() {
+        return mMinute;
+    }
+
+    public void setMinute(int minute) {
+        mMinute = minute;
+    }
+
+    public int getSecond() {
+        return mSecond;
+    }
+
+    public void setSecond(int second) {
+        mSecond = second;
+    }
+    
+}

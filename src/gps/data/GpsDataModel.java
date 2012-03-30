824c8f3cb2f2f39931fcf811f34d45109f7de77c
javarobots74 <javarobots74@gmail.com>
2012-03-29 19:01:32 -0600

    Updated variables for GGA, RMC and GSA messages

--- src/gps/data/GpsDataModel.java
+++ src/gps/data/GpsDataModel.java
@@ -10,23 +10,35 @@ import java.util.Observable;
 public class GpsDataModel extends Observable {
 
     //From $GPGGA sentence
-    private double mGGALatitude = 00.000;
-    private double mGGALongitude = 000.000;
-    private String mGGALatitudeHemisphere = "N"; //N or S
-    private String mGGALongitudeHemispere = "W"; //E or W
+    private UTCTime mGGATime;
+    private Coordinate mGGACoordinate;
     private int mGGAFixQuality = 0; //0=Invalid 1=GPS 2=DGPS
     private double mGGAAltitude = 0; //Altitude in meters
     private int mGGANumberOfSatelites = 0; //Number of satellites being tracked
+    private double mGGAHdop = 99;
+    private double mGGAHeightAboveSeaLevel = 0;
+    private double mGGAGeoidalHeight = 0;
 
     //From $GPRMC sentence
+    private UTCTime mRMCTime;
+    private String mRMCStatus = "V";
+    private Coordinate mRMCCoordinate;
     private double mRMCSpeedOverGround = 0; //Speed in knots
-    private double mRMCTrueCourse = 0; //Heading over ground
+    private double mRMCCourseOverGround = 0; //Heading over ground
+    private UTCDate mRMCDate;
+    private double mRMCMagneticVariation;
+    private String mRMCMagneticVariationDirection;
+    private String mRMCModeIndicator = "N";
 
     //From $GPGSA sentence
-    private int mGSAFixMode = 1; //1=No Fix 2=2D 3=3D
+    private String mGSAMode = "M";
+    private int mGSAFixType = 1; //1=No Fix 2=2D 3=3D
+    private int[] mGSAPRNNumber = {0,0,0,0,0,0,0,0,0,0,0,0};
     private double mGSAPdop = 99;
     private double mGSAHdop = 99;
     private double mGSAVdop = 99;
+    
+    //---------------------- GGA Getter Setters ----------------------------
 
     public double getGgaAltitude() {
         return mGGAAltitude;
@@ -37,21 +49,23 @@ public class GpsDataModel extends Observable {
         setChanged();
     }
 
-    public int getGsaFixMode() {
-        return mGSAFixMode;
-    }
-
-    public void setGsaFixMode(int fixMode) {
-        this.mGSAFixMode = fixMode;
-        setChanged();
-    }
-
     public int getGgaFixQuality() {
         return mGGAFixQuality;
     }
 
     public void setGgaFixQuality(int fixQuality) {
         this.mGGAFixQuality = fixQuality;
+        setChanged();
+    }
+    
+    //---------------------- GSA Getter Setter -----------------------------    
+
+    public int getGsaFixMode() {
+        return mGSAFixType;
+    }
+
+    public void setGsaFixMode(int fixMode) {
+        this.mGSAFixType = fixMode;
         setChanged();
     }
 
@@ -64,66 +78,12 @@ public class GpsDataModel extends Observable {
         setChanged();
     }
 
-    public double getGgaLatitude() {
-        return mGGALatitude;
-    }
-
-    public void setGgaLatitude(double latitude) {
-        this.mGGALatitude = latitude;
-        setChanged();
-    }
-
-    public String getGgaLatitudeHemisphere() {
-        return mGGALatitudeHemisphere;
-    }
-
-    public void setGgaLatitudeHemisphere(String latitudeHemisphere) {
-        this.mGGALatitudeHemisphere = latitudeHemisphere;
-        setChanged();
-    }
-
-    public double getGgaLongitude() {
-        return mGGALongitude;
-    }
-
-    public void setGgaLongitude(double longitude) {
-        this.mGGALongitude = longitude;
-        setChanged();
-    }
-
-    public String getGgaLongitudeHemispere() {
-        return mGGALongitudeHemispere;
-    }
-
-    public void setGgaLongitudeHemispere(String longitudeHemispere) {
-        this.mGGALongitudeHemispere = longitudeHemispere;
-        setChanged();
-    }
-
     public double getGsaPdop() {
         return mGSAPdop;
     }
 
     public void setGsaPdop(double pdop) {
         this.mGSAPdop = pdop;
-        setChanged();
-    }
-
-    public double getRmcSpeedOverGround() {
-        return mRMCSpeedOverGround;
-    }
-
-    public void setRmcSpeedOverGround(double speedOverGround) {
-        this.mRMCSpeedOverGround = speedOverGround;
-        setChanged();
-    }
-
-    public double getRmcTrueCourse() {
-        return mRMCTrueCourse;
-    }
-
-    public void setRmcTrueCourse(double trueCourse) {
-        this.mRMCTrueCourse = trueCourse;
         setChanged();
     }
 
@@ -144,6 +104,24 @@ public class GpsDataModel extends Observable {
         this.mGGANumberOfSatelites = numberOfSatelites;
         setChanged();
     }
+    
+    //---------------------- RMC Getter Setter -----------------------------
 
+    public double getRmcSpeedOverGround() {
+        return mRMCSpeedOverGround;
+    }
 
+    public void setRmcSpeedOverGround(double speedOverGround) {
+        this.mRMCSpeedOverGround = speedOverGround;
+        setChanged();
+    }
+
+    public double getRmcTrueCourse() {
+        return mRMCCourseOverGround;
+    }
+
+    public void setRmcTrueCourse(double trueCourse) {
+        this.mRMCCourseOverGround = trueCourse;
+        setChanged();
+    }
 }

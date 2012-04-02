b7ac8085cce964d5e6a9afb75b276ce507e5da3e
javarobots74 <javarobots74@gmail.com>
2012-04-02 06:11:00 -0600

    Corrected issue with sentence header

--- src/gps/nmea/SentenceParser.java
+++ src/gps/nmea/SentenceParser.java
@@ -11,7 +11,6 @@ public class SentenceParser {
 
     private GpsDataModel mDataModel;
     private boolean mBeginOfSentence = false;
-    private StringBuilder mCompleteSentence;
 
     public SentenceParser(GpsDataModel model){
         mDataModel = model;
@@ -19,7 +18,6 @@ public class SentenceParser {
 
     public String[] processData(String data){
         if (!mBeginOfSentence){
-            mCompleteSentence = new StringBuilder();
             int indexOfStart = data.indexOf("$");
             if (indexOfStart != -1){
                 data = data.substring(indexOfStart);
@@ -30,7 +28,7 @@ public class SentenceParser {
         String[] sentences = data.split("[$]");
         for (String sentence : sentences){
             if (!sentence.isEmpty()){
-                parseSentence(sentence);
+                parseSentence("$" + sentence);
             }
         }
         return sentences;
@@ -39,17 +37,17 @@ public class SentenceParser {
     public void parseSentence(String sentence) {
         sentence = sentence.trim();
         String[] splitMessage = sentence.split(",");
-        if (splitMessage[0].equals("GPGSA")){
+        if (splitMessage[0].equals("$GPGSA")){
             parseGsa(sentence);
-        } else if (splitMessage[0].equals("GPRMC")){
+        } else if (splitMessage[0].equals("$GPRMC")){
             parseRmc(sentence);
-        } else if (splitMessage[0].equals("GPGGA")){
+        } else if (splitMessage[0].equals("$GPGGA")){
             parseGga(sentence);
-        } else if (splitMessage[0].equals("GPGLL")){
+        } else if (splitMessage[0].equals("$GPGLL")){
 
-        } else if (splitMessage[0].equals("GPGSV")){
+        } else if (splitMessage[0].equals("$GPGSV")){
 
-        } else if (splitMessage[0].equals("GPVTG")){
+        } else if (splitMessage[0].equals("$GPVTG")){
             parseVtg(sentence);
         }
     }
@@ -63,12 +61,12 @@ public class SentenceParser {
 
         //Ensure message is proper length
         if (values.length == 15){
-            
+
             //Set GGA UTC time
             UTCTime time = parseUtcTime(values[1]);
-            mDataModel.setGGATime(time);                        
+            mDataModel.setGGATime(time);
 
-            //Set GGA Coordinate            
+            //Set GGA Coordinate
             Coordinate ggaCoordinate = parseCoordinate(values[2], values[3], values[4], values[5]);
             mDataModel.setGGACoordinate(ggaCoordinate);
 
@@ -77,13 +75,13 @@ public class SentenceParser {
             if (!fixQuality.isEmpty()){
                 mDataModel.setGgaFixQuality(Integer.parseInt(fixQuality));
             }
-            
+
             //Set number of stellites in use
             String numberOfSatelites = values[7];
             if (!numberOfSatelites.isEmpty()){
                 mDataModel.setGGANumberOfSatelites(Integer.parseInt(numberOfSatelites));
             }
-            
+
             //Set HDOP
             String hdop = values[8];
             if (!hdop.isEmpty()){
@@ -94,9 +92,9 @@ public class SentenceParser {
             if (!altitude.isEmpty()){
                 mDataModel.setGGAHeightAboveSeaLevel(Double.parseDouble(altitude));
             }
-            
+
             //Set Geoidal height
-            String geodialHeight = values[10];
+            String geodialHeight = values[11];
             if (!geodialHeight.isEmpty()){
                 try{
                     mDataModel.setGGAGeoidalHeight(Double.parseDouble(geodialHeight));
@@ -115,61 +113,61 @@ public class SentenceParser {
      */
     private void parseRmc(String message){
         String[] values = message.split(",");
-        if (values.length == 13){            
-            
+        if (values.length == 13){
+
             //Set UTC Time
             String utcTime = values[1];
             if (!utcTime.isEmpty()){
                 UTCTime time = parseUtcTime(utcTime);
                 mDataModel.setRMCTime(time);
             }
-            
+
             //Set status
             String status = values[2];
             if (!status.isEmpty()){
                 mDataModel.setRMCStatus(status);
             }
-            
+
             //Set coordinate
             Coordinate coordinate = parseCoordinate(values[3], values[4], values[5], values[6]);
             mDataModel.setRMCCoordinate(coordinate);
-            
+
             //Set speed over ground
             String speed = values[7];
             if (!speed.isEmpty()){
                 mDataModel.setRmcSpeedOverGround(Double.parseDouble(speed));
             }
-            
+
             //Set course over ground
             String course = values[8];
             if (!course.isEmpty()){
                 mDataModel.setRmcTrueCourse(Double.parseDouble(course));
             }
-            
+
             //Set the UTC date
             String date = values[9];
             if (!date.isEmpty()){
                 mDataModel.setRMCDate(parseUtcDate(date));
             }
-            
+
             //Set magnetic variation
             String variation = values[10];
             if (!variation.isEmpty()){
                 mDataModel.setRMCMagneticVariation(Double.parseDouble(variation));
             }
-            
+
             //Set magentic variation direcion
             String varDirection = values[11];
             if (!varDirection.isEmpty()){
                 mDataModel.setRMCMagneticVariationDirection(varDirection);
             }
-            
+
             //Set mode indicator
             String mode = values[12];
             if (!mode.isEmpty()){
                 mDataModel.setRMCModeIndicator(mode);
             }
-            
+
         }
     }
 
@@ -212,18 +210,44 @@ public class SentenceParser {
     private void parseVtg(String message) {
         String[] values = message.split(",");
         if (values.length == 9){
-            
-            //Set ground speed
-            String groundSpeed = values[7];
-            if (!groundSpeed.isEmpty()){
-                mDataModel.setRmcSpeedOverGround(Double.parseDouble(groundSpeed));
+            //Set true course over ground
+            String trueCourse = values[1];
+            if (!trueCourse.isEmpty()){
+                mDataModel.setVTGTrueCourse(Double.parseDouble(trueCourse));
             }
+
+            //Set magnetic course
+            String magneticCourse = values[3];
+            if (!magneticCourse.isEmpty()){
+                mDataModel.setVTGMagneticCourse(Double.parseDouble(magneticCourse));
+            }
+
+            //Set ground speed in knots
+            String speedInKnots = values[5];
+            if (!speedInKnots.isEmpty()){
+                mDataModel.setVTGSpeedInKnots(Double.parseDouble(speedInKnots));
+            }
+
+            //Set ground speed in KPH
+            String speedInKPH = values[7];
+            if (!speedInKPH.isEmpty()){
+                mDataModel.setVTGSpeedInKilometers(Double.parseDouble(speedInKPH));
+            }
+
+            //!jdp - the mode indicator value is not present from etrx legend
+            //Set mode indicator
+//            String modeIndicator = values[9];
+//            if (!modeIndicator.isEmpty()){
+//                String[] splitMode = modeIndicator.split("*");
+//                mDataModel.setVTGModeIndicator(splitMode[0]);
+//            }
+
         }
     }
-    
+
     //!jdp -- Need to add error correction
     private UTCTime parseUtcTime(String utcTime){
-        
+
         //Parse time from end of utcTime String
         String utcSecond = utcTime.substring(utcTime.length() - 2);
         String utcMinutes = utcTime.substring(utcTime.length() - 4, utcTime.length() - 2);
@@ -232,28 +256,28 @@ public class SentenceParser {
                 Integer.parseInt(utcHour),
                 Integer.parseInt(utcMinutes),
                 Integer.parseInt(utcSecond));
-        
+
         return time;
     }
-    
+
     private UTCDate parseUtcDate(String utcDate){
-        
+
         //Parse date from end
         String utcYear = utcDate.substring(utcDate.length() - 2);
         String utcMonth = utcDate.substring(utcDate.length() - 4, utcDate.length() - 2);
         String utcDay = utcDate.substring(0, utcDate.length() - 4);
-        
+
         UTCDate date = new UTCDate(Integer.parseInt(utcDay), Integer.parseInt(utcMonth), Integer.parseInt(utcYear));
-        return date;        
+        return date;
     }
-    
+
     //!jdp -- Need to add error correction
     private Coordinate parseCoordinate(
             String latitude,
             String latitudeHemisphere,
             String longitude,
             String longitudeHemisphere){
-            
+
             Hemisphere latHemisphere;
             Hemisphere lonHemisphere;
             if (latitudeHemisphere.equals("N")){
@@ -261,13 +285,13 @@ public class SentenceParser {
             } else {
                 latHemisphere = Hemisphere.SOUTH;
             }
-            
+
             if (longitudeHemisphere.equals("E")){
                 lonHemisphere = Hemisphere.EAST;
             } else {
                 lonHemisphere = Hemisphere.WEST;
             }
-            
+
             Coordinate coordinate = new Coordinate(
                     Double.parseDouble(latitude),
                     latHemisphere,

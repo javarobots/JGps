5bc2f3313c0daacee1106526ed80b811e078523f
javarobots74 <javarobots74@gmail.com>
2012-04-02 20:34:49 -0600

    Completed checksum class. Given a NMEA sentence it can be checked for validity.

--- src/gps/nmea/Checksum.java
+++ src/gps/nmea/Checksum.java
@@ -1,31 +1,42 @@
 
 package gps.nmea;
 
-import java.util.zip.CRC32;
-
 /**
  *
  * @author javarobots
  */
 public class Checksum {
-
-        public static void main(String[] args){
-
-            String sentence = "$GPVTG,139.4,T,139.4,M,0.14,N,0.26,K*4F";
-            int astriskIndex = sentence.indexOf("*");
-            int checksumValue = 0;
-            System.out.println("Checksum marker: " + astriskIndex);
-            for (int i = 1; i < astriskIndex; i++){
-                System.out.print(sentence.charAt(i));
-                checksumValue = checksumValue ^ Character.getNumericValue(sentence.charAt(i));
-            }
-            System.out.println("\nValues to calculate");
-            System.out.println("Checksum value: " + checksumValue);
-
-            CRC32 crc = new CRC32();
-            byte[] bytes = sentence.getBytes();
-            crc.update(bytes, 1, astriskIndex - 1);
-            System.out.println("CRC: " + crc.getValue());
+    
+    private String mSentence;
+    
+    public Checksum(String sentence){
+        mSentence = sentence;
+    }
+    
+    public boolean isValid(){
+        boolean valid = true;
+        byte[] bytes = mSentence.getBytes();
+        int astriskIndex = mSentence.indexOf("*");
+        byte checksumValue = 0;
+        int checksumFromSentence;
+        
+        //Check to see if first character is $
+        if (mSentence.charAt(0) != '$'){
+            valid = false;
         }
+        
+        //If valid calculate checksum
+        if (valid){
+            String val = mSentence.substring(astriskIndex + 1, mSentence.length());
+            checksumFromSentence = Integer.parseInt(val, 16);
+            for (int i = 1; i < astriskIndex; i++){
+                checksumValue = (byte) (checksumValue ^ bytes[i]);
+            }
+            if (checksumFromSentence != checksumValue){
+                valid = false;
+            }
+        }        
+        return valid;
+    }
 
 }
